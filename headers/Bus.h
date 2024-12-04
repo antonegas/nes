08169c6dde0fc0da67c6d3cdd69b3f9842fd038e
antonegas <32789589+antonegas@users.noreply.github.com>
2024-12-04 14:59:06 +0100

    Added more external components to bus

--- headers/Bus.h
+++ headers/Bus.h
@@ -3,13 +3,22 @@
 
 #include <cstdint>
 
+#include "CPU.h"
+#include "PPU.h"
+#include "APU.h"
+
 class Bus {
     public:
+        // TODO: Bus constructor should set the bus pointer of the cpu to itself
+        // TODO: Make sure that read and write access the correct register/memory as it can be different between read and write
         uint8_t read(uint16_t address);
         void write(uint16_t address, uint8_t data);
         void clock(); 
     private:
         uint8_t ram[2048];
+        CPU cpu;
+        PPU ppu;
+        APU apu;
 
         // Controllers
         // TODO: probably change to an abstract "controller" class which can implement more controllers than the normal NES controller.
