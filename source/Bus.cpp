79d4c03fdf64e0ae91561ca9f6b32cd198c0a172
antonegas <32789589+antonegas@users.noreply.github.com>
2025-10-14 09:54:13 +0200

    More format

--- source/Bus.cpp
+++ source/Bus.cpp
@@ -28,23 +28,23 @@ void Bus::tick() {
 }
 
 uint8_t Bus::read(uint16_t address) {
-    uint16_t masked_address; // Used when addresses are repeated
+    uint16_t masked_address; // Used when addresses are repeated.
 
     if (address <= 0x1FFF) {
         // RAM
         return ram[address & 0x7FF];
     } else if (address <= 0x3FFF) {
-        // PPU registers
+        // PPU registers.
         masked_address = address & 0x2007;
         return ppu.read(masked_address);
     } else if (address <= 0x4015) {
-        // APU and I/O registers
+        // APU and I/O registers.
         // TODO: 0x4017 can also be an APU address
         return apu.read(address);
     } else if (address <= 0x4017) {
         switch (address) {
         case 0x4016:
-            // based on standard nes controller
+            // Based on standard nes controller.
             bool data = (controller1 & 0x80) > 0;
             controller1 = controller1 << 1;
             return data;
@@ -64,7 +64,7 @@ uint8_t Bus::read(uint16_t address) {
 }
 
 void Bus::write(uint16_t address, uint8_t data) {
-    uint16_t masked_address; // Used when addresses are repeated
+    uint16_t masked_address; // Used when addresses are repeated.
 
     if (address <= 0x1FFF) {
         // RAM
@@ -75,7 +75,7 @@ void Bus::write(uint16_t address, uint8_t data) {
         ppu.write(masked_address, data);
     } else if (address <= 0x4015) {
         // TODO: move to APU and I/O classes
-        // APU and I/O registers
+        // APU and I/O registers.
         // TODO: 0x4017 can also be an APU address
         apu.write(address, data);
     } else if (address <= 0x4017) {
