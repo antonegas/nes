7b1fbfbe12697b4fa92a2e965c0e08e11d9bda79
antonegas <32789589+antonegas@users.noreply.github.com>
2025-10-15 16:01:18 +0200

    Started on PPU registers

--- headers/PPU.h
+++ headers/PPU.h
@@ -12,15 +12,90 @@ class PPU {
         uint8_t read(uint16_t address);
         void write(uint16_t address, uint8_t data);
     private:
-        // PPU registers
-        uint8_t ppuctrl;
-        uint8_t ppumask;
-        uint8_t ppustatus;
-        uint8_t oamaddr;
-        uint8_t oamdata;
-        uint16_t ppuscroll;
-        uint16_t ppuaddr;
-        uint8_t ppudata;
+        /**
+         * PPU REGISTERS
+         * 
+         * The PPU has some registers which can be written to and/or read from by the CPU: PPUCTRL, 
+         * PPUMASK, PPUSTATUS, OAMADDR, OAMDATA, PPUSCROLL, PPUADDR, PPUDATA and OAMDMA.
+         * 
+         * PPUCTRL (VPHBSINN, write only): NMI enable (V), master slave/unused (P), sprite height 8x8 or 8x16 (H), 
+         * background tile select (B), sprite tile select (S), increment mode going across or down (I)
+         * and nametable select.
+         * 
+         * PPUMASK (BGRsbMmG, write only): enhanced blue/green/red (BGR), enable sprite (s), enable background (b),
+         * sprite enable in left column (M), background enable in left column (m) and grayscale (G).
+         * 
+         * PPUMASK (VSO-----, read only): vblank (V), sprite 0 hit (S) and sprite overflow (O).
+         * 
+         * OAMADDR (write only): Sprite RAM address.
+         * OAMADATA (write only): Sprite RAM data.
+         * PPUSCROLL (write only): X scroll then Y scroll with two writes.
+         * PPUADDR (write only): Address where CPU will write to/read from.
+         * PPUDATA (write): Writes to the PPUADDR and increments based on PPUCTRL increment mode.
+         * PPUDATA (read): A buffered response for the given address, one read behind.
+         * OAMDMA (write): Suspends CPU to allow PPU to copy a page from its memory at address 0xXX00.
+         * 
+         * Reference: https://www.nesdev.org/wiki/PPU_registers
+         */
+
+        union PPUCTRL {
+            struct {
+                uint8_t nametableSelect : 2;
+                uint8_t incrementMode : 1;
+                uint8_t spriteTileSelect : 1;
+                uint8_t backgroundTileSelect : 1;
+                uint8_t spriteHeight : 1;
+                uint8_t unused : 1;
+                uint8_t nmiEnable : 1;
+            };
+            uint8_t reg = 0x00;
+        } ppuctrl;
+
+        union PPUMASK {
+            struct {
+                uint8_t grayscale : 1;
+                uint8_t backgroundLeft : 1;
+                uint8_t spriteLeft : 1;
+                uint8_t enableBackground : 1;
+                uint8_t enableSprite : 1;
+                uint8_t emphasizeRed : 1;
+                uint8_t emphasizeGreen : 1;
+                uint8_t emphasizeBlue : 1;
+            };
+            uint8_t reg = 0x00;
+        } ppumask;
+
+        union PPUSTATUS {
+            struct {
+                uint8_t unused : 5;
+                uint8_t O : 1;
+                uint8_t S : 1;
+                uint8_t V : 1;
+            };
+            uint8_t reg = 0x00;
+        };
+
+        uint8_t oamaddr = 0x00;
+        uint8_t oamdata = 0x00;
+        uint16_t ppuscroll = 0x0000;
+        uint16_t ppuaddr = 0x0000;
+        // uint8_t ppudata; // NOTE: not a real address. Writes to vram at ppuaddr.
+        uint8_t oamdma = 0x00;
+
+        // Latches for double write registers.
+        bool ppuscrollLatch = false;
+        bool ppuaddrLatch = false;
+
+        // Last PPUSCROLL/PPUADDR writes.
+        uint8_t ppuscrollLast = 0x00;
+        uint8_t ppuaddrLast = 0x00;
+
+        // Odd frame indicator.
+        bool oddFrame = false;
+
+        /**
+         * PPU MEMORY MAP
+         */
 };
 
 #endif // H_PPU
