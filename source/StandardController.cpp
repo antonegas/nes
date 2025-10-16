137aca1c980ec46f05432596da595f88072185a2
antonegas <32789589+antonegas@users.noreply.github.com>
2025-10-16 10:04:28 +0200

    Fixed wrong bit being set

--- source/StandardController.cpp
+++ source/StandardController.cpp
@@ -8,7 +8,7 @@ using std::uint8_t;
 uint8_t StandardController::read() {
     if (!remaining) return 0x01;
 
-    uint8_t bit = buffer & 0x80; // Capture the bit before shifting.
+    bool bit = (buffer & 0x80); // Capture the bit before shifting.
     buffer = buffer << 1;
     remaining--;
 
