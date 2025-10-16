8baf93d293c30fd6feb092f5102a63585f845015
antonegas <32789589+antonegas@users.noreply.github.com>
2025-10-16 12:27:32 +0200

    Added addresses to PPU read/write

--- source/PPU.cpp
+++ source/PPU.cpp
@@ -7,32 +7,42 @@ using std::uint8_t;
 
 uint8_t PPU::read(uint16_t address) {
     switch (address) {
-    case 0x2000:
-        return ppuctrl;
-        break;
-    case 0x2001:
-        return ppumask;
-        break;
-    case 0x2002:
-        return ppustatus;
-        break;
-    case 0x2003:
-        return oamaddr;
-        break;
-    case 0x2004:
-        return oamdata;
-        break;
-    case 0x2005:
-        return ppuscroll;
-        break;
-    case 0x2006:
-        return ppuaddr;
-        break;
-    case 0x2007:
-        return ppudata;
-        break;
-    default:
-        return 0x00;
-        break;
+        case 0x2002:
+            // TODO: PPUSTATUS
+            return 0x00;
+        case 0x2004:
+            // TODO: OAMDATA
+            return 0x00;
+        case 0x2007:
+            // TODO: PPUDATA
+            return 0x00;
+        default:
+            return 0x00;
+    }
+}
+
+void PPU::write(uint16_t address, uint8_t data) {
+    switch (address) {
+        case 0x2000:
+            // TODO: PPUCTRL
+            return;
+        case 0x2001:
+            // TODO: PPUMASK
+            return;
+        case 0x2003:
+            // TODO: OAMAADDR
+            return;
+        case 0x2004:
+            // TODO: OAMADATA
+            return;
+        case 0x2005:
+            // TODO: PPUSCROLL
+            return;
+        case 0x2006:
+            // TODO: PPUADDR
+            return;
+        case 0x2007:
+            // TODO: PPUDATA
+            return;
     }
 }
