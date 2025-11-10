74ee403d23809eaf36c8e25a7fb70681860e85a3
antonegas <32789589+antonegas@users.noreply.github.com>
2025-11-10 20:06:05 +0100

    Stylistic change

--- source/StandardController.cpp
+++ source/StandardController.cpp
@@ -7,7 +7,7 @@ using std::uint8_t;
 uint8_t StandardController::read() {
     if (!remaining) return 0x01;
 
-    bool bit = (buffer & 0x80); // Capture the bit before shifting.
+    bool bit = buffer & 0x80; // Capture the bit before shifting.
     buffer = buffer << 1;
     remaining--;
 
