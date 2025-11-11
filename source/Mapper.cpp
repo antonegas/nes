581f983703bca88f8cbb8076288320308ab56b33
antonegas <32789589+antonegas@users.noreply.github.com>
2025-11-12 00:33:44 +0100

    Fixed vertical mirroring

--- source/Mapper.cpp
+++ source/Mapper.cpp
@@ -8,8 +8,8 @@ using std::uint16_t;
 uint16_t Mapper::mirrorAddr(uint16_t addr) {
     switch (mirrorMode) {
         case NametableLayout::VERTICAL:
-            if (addr & 0x0800) return addr & 0x03FF;
-            else return (addr & 0x03FF) | 0x0400;
+            if (addr & 0x0800) return (addr & 0x03FF) | 0x0400;
+            else return addr & 0x03FF;
         case NametableLayout::HORIZONTAL:
             return addr & 0x07FF;
         case NametableLayout::FOUR:
