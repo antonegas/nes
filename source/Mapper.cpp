00fbc8367d789df8405cf4588b8e7b05066d1d47
antonegas <32789589+antonegas@users.noreply.github.com>
2025-11-14 15:48:00 +0100

    Fixed wrong mirroring

--- source/Mapper.cpp
+++ source/Mapper.cpp
@@ -7,10 +7,10 @@ using std::uint16_t;
 
 uint16_t Mapper::mirrorAddr(uint16_t addr) {
     switch (mirrorMode) {
-        case NametableLayout::VERTICAL:
+        case NametableLayout::HORIZONTAL:
             if (addr & 0x0800) return (addr & 0x03FF) | 0x0400;
             else return addr & 0x03FF;
-        case NametableLayout::HORIZONTAL:
+        case NametableLayout::VERTICAL:
             return addr & 0x07FF;
         case NametableLayout::FOUR:
             return addr & 0x0FFF;
