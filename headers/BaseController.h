a7bb489fc6f62b6929b6280a6e87812c930ca9f3
Anton Nilsson <32789589+antonegas@users.noreply.github.com>
2026-08-19 21:26:48 +0200

    Added restrictions to BaseController

--- headers/BaseController.h
+++ headers/BaseController.h
@@ -17,9 +17,16 @@ class BaseController {
          * 
          * Reference: https://www.nesdev.org/wiki/Standard_controller
          */
+        // NOTE: The controller port has seven pins 2 output (OUT and CLK), 3 output (D0, D3 and D4) a 5V and GND.
+        // NOTE: CLK is a falling edge which is set low when a read is done on the port.
+        // NOTE: OUT is the D0 bit from the write bus.
+        // TODO: Update documentation (https://www.nesdev.org/wiki/Controller_port_pinout) is a better reference.
 
-        virtual uint8_t read() { return 0x00; };
-        virtual void reload() {};
+        virtual uint8_t read(uint16_t) { return clk() & 0x19; }
+        virtual void write(uint16_t, uint8_t data) { if (data & 0x01) out(); }
+    private:
+        virtual void out() {}
+        virtual uint8_t clk() { return 0x00; }
 };
 
 #endif // H_BASE_CONTROLLER
